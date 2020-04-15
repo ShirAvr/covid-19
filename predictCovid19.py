b2ce01a0b99abb4acc498dc665ebfc6c68a7bed5
ShirAvr <shir@bhh.co.il>
2020-04-15 18:20:45 +0300

    Use date type as toordinal

--- predictCovid19.py
+++ predictCovid19.py
@@ -4,12 +4,14 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
 df_patients = pd.read_excel(open('corona_tested_individuals.xlsx', 'rb'), sheet_name='1 - tested person data') 
 rowsCount, colCount = df_patients.shape
 print(rowsCount)
 
 df_patients = df_patients.dropna()
+print(df_patients.dtypes)
 
 # 1 for positive to corona test, 0 for negative
 # 1 for female, 0 for male
@@ -17,19 +19,22 @@ df_patients = df_patients.dropna()
 df_patients['corona_result'] = np.where(df_patients['corona_result']=='שלילי', 0, 1)
 df_patients['gender'] = np.where(df_patients['gender']=='זכר', 0, 1)
 df_patients['age_60_and_above'] = np.where(df_patients['age_60_and_above']=='No', 0, 1)
+df_patients['test_date'] = pd.to_datetime(df_patients.test_date)
+df_patients['test_date'] = df_patients.test_date.apply(lambda x: x.toordinal())
 
 rowsCount, colCount = df_patients.shape
 print(rowsCount)
 print(df_patients.head())
 print(df_patients.tail())
 print(df_patients.describe())
+print(df_patients.dtypes)
 
 df_inputs = df_patients.drop(['corona_result'], axis=1) # seperate the depended param from the independed data
 
 print(df_inputs.head())
 print(df_inputs.tail())
 
-x = df_inputs.iloc[:, :].values 	# inputs
+x = df_inputs.values 	# inputs
 y = df_patients.iloc[:, 6].values # outputs
 
 print("=====")
@@ -38,6 +43,13 @@ print(y)
 print("=====")
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+
+scaler = StandardScaler()
+scaler.fit(x_train)
+
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
 classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(x_train, y_train)
 y_pred = classifier.predict(x_test)
