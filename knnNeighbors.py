d8e806b58fd1cad2a10502f302060a2838511986
ShirAvr <shir@bhh.co.il>
2020-04-28 02:02:37 +0300

    Add logistic regression algorithm

--- knnNeighbors.py
+++ knnNeighbors.py
@@ -43,9 +43,9 @@ print("=====")
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
 
+# performs feature scaling
 scaler = StandardScaler()
 scaler.fit(x_train)
-
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
 
