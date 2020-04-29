254c530b9d7446986d643d9ec1707daa248c06c6
ShirAvr <shir@bhh.co.il>
2020-04-29 14:51:14 +0300

    Add roc curve to validates models

--- knnNeighbors.py
+++ knnNeighbors.py
@@ -5,7 +5,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, balanced_accuracy_score
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt 
 
 x_train, x_test, y_train, y_test = prepareData.get()
 
@@ -22,3 +24,19 @@ print(y_pred)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 print(balanced_accuracy_score(y_test, y_pred))
+
+y_scores = knnClassifier.predict_proba(x_test)
+fpr, tpr, threshold = roc_curve(y_test, y_scores[:, 1])
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+plt.plot(fpr, tpr, 'b', label = 'AUC (area = %0.2f)' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve for knn')
+plt.savefig('knn_roc')
+plt.show()
--- logisticRegression.py
+++ logisticRegression.py
@@ -4,7 +4,9 @@ import prepareData
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, balanced_accuracy_score
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+import matplotlib.pyplot as plt 
 
 x_train, x_test, y_train, y_test = prepareData.get()
 
@@ -23,3 +25,17 @@ y_pred = logreg.predict(x_test)
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 print(balanced_accuracy_score(y_test, y_pred))
+
+logit_roc_auc = roc_auc_score(y_test, y_pred)
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(x_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC curve for logistic')
+plt.legend(loc="lower right")
+plt.savefig('Log_roc')
+plt.show()
